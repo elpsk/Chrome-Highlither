fa7b62e14edf833bff886437cd1a9a95a30c0ecc
paska <alberto.pasca@gmail.com>
2012-03-26 11:03:25 +0200

    first commit

---
+++ contentScript.js
@@ -0,0 +1,14 @@
+
+document.body.innerHTML = document.body.innerHTML.replace(/(A.*?\b)/g, "<font color='red'>$1</font>");
+
+
+/*
+// TODO -> APPLY A SYLE ON DEFINITED WORDS ON PAGE.
+//      -> SHOW RIGHT CLICK MENU ONLY ON THESE WORDS
+
+City = new Array("Milano", "Roma", "Napoli", "Bologna", "Venezia");
+for (idx=0; idx<City.length; idx++) {
+	document.body.innerHTML = 
+		document.body.innerHTML.replace(City[idx], "<font color='red'>"+City[idx]+"</font>");
+}
+*/
---
+++ rightclick.js
@@ -0,0 +1,22 @@
+
+/*
+
+// SCRIPT TO SHOW A RIGHT-MENU.
+// ************
+// TODO-> show right menu only on a definited array words!
+// ************
+
+function sendSearch(selectedText) {
+	var serviceCall = 'http://maps.google.it/q=' + selectedText;
+	chrome.tabs.create({url: serviceCall});
+}
+
+chrome.contextMenus.create({title: "Search '%s' maps!", contexts:["selection"], onclick: function(info, tab) {
+  sendSearch(info.selectionText);
+}});
+*/
+
+
+chrome.tabs.getSelected(null, function(tab { 
+	chrome.tabs.executeScript(tab.id, {file:'contentScript.js'});
+})); 
